dcaaccb825a35bd0f06f0e16c9f70b00dc3c3432
Sergio Cano <li_sergiocm@unca.edu.mx>
2021-06-30 19:38:41 -0500

    variables en PHP

---
+++ variables.php
@@ -0,0 +1,22 @@
+<?php
+ 
+    define('ConstanteNum', 10);
+    define('Constante', 'Soy un constante');
+    $numero = 10;
+    $band = true;
+    $decimal = 1.23;
+    $cadena = 'HOlA MUNDO';
+    $arreglo = array('a','b',1,2.123);
+
+
+    echo $numero.'<br>';
+    echo $band.'<br>';
+    echo $decimal.'<br>';
+    echo $cadena.'<br>';
+    echo Constante;
+
+    echo ConstanteNum;
+
+    print_r($arreglo);
+
+?>
